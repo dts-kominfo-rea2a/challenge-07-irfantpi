7bf4944260f5ac9fa38280cdbbdccd75801d4e19
Irfan Triputra Isyan <irfantpi.114@gmail.com>
2022-06-12 19:09:43 +0700

    challenge-07

--- promise.js
+++ promise.js
@@ -1,7 +1,18 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    let promise1 = await promiseTheaterIXX();
+    let promise2 = await promiseTheaterVGC();
+    let hasil = (promise1).concat(promise2);
+    let jumlahEmosi = 0;
+    hasil.map((promise1) => promise1.hasil === emosi && (jumlahEmosi++));
+    return jumlahEmosi;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
